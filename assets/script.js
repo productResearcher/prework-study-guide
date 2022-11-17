1273a61ef095c4e15b9b7d561f5d836d21e99984
ProductResearcher <abdulkadirhaldu@gmail.com>
2022-11-17 19:46:14 +0000

    pushing the unsaved JS list items, and JS file

--- assets/script.js
+++ assets/script.js
@@ -21,6 +21,7 @@ function selectTopic() {
  }
 }
 
-listTopics()
-selectTopic()
-
+console.log('Here are the topics we learned through Prework:');
+listTopics();
+console.log('Which topic should we study first?');
+selectTopic();
